6b8afd9aa55381ab374a605bc1c46153440cec7f
vhlebnikov <v.khlebnikov@g.nsu.ru>
2024-04-29 01:40:26 +0700

    Task_3 done

--- Task_3/src/main/c/nsu_Methods.c
+++ Task_3/src/main/c/nsu_Methods.c
@@ -3,19 +3,25 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <windows.h>
+#include <unistd.h>
 
 JNIEXPORT void JNICALL Java_nsu_Methods_memoryEater(JNIEnv *env, jobject obj) {
     while (1) {
-        int* arr = (int*) malloc(sizeof(int) * INT_MAX);
-        for (int i = 0; i < INT_MAX; i++) {
-            arr[i] = i;
+        size_t size = sizeof(int) * INT_MAX;
+        int* arr = (int*) malloc(size);
+        if (arr != NULL) {
+            memset(arr, 0, size);
         }
     }
 };
 
 JNIEXPORT jlong JNICALL Java_nsu_Methods_allocateOneKB(JNIEnv *env, jobject obj) {
-    int64_t* array = (int64_t*) malloc(sizeof(int64_t) * 128);
-    return (jlong) array;
+    char* arr = (char*) malloc(1024);
+    if (arr != NULL) {
+        memset(arr, 0, 1024);
+    }
+    return (jlong) arr;
 };
 
 void a(JNIEnv *env);
@@ -27,12 +33,18 @@ void c(JNIEnv *env) {
         count++;
         a(env);
     } else {
-        jclass Exception = (*env)->FindClass(env, "java/lang/Exception");
+//        int *ptr = NULL;
+//        *ptr = 1;
+        jclass exceptionClass = (*env)->FindClass(env, "java/lang/Exception");
 
-        int res = (*env)->ThrowNew(env, Exception, "MY EXCEPTION");
+        if (exceptionClass == NULL) {
+            return;
+        }
+
+        int res = (*env)->ThrowNew(env, exceptionClass, "MY EXCEPTION");
         if (res >= 0) {
             printf("Exception was throwed\n");
-
+            (*env)->ExceptionDescribe(env);
         }
         return;
     }
@@ -75,14 +87,29 @@ JNIEXPORT void JNICALL Java_nsu_Methods_changeObjectField(JNIEnv *env, jobject o
     (*env)->SetIntField(env, class_object, id, val);
 };
 
-//JNIEXPORT jlong JNICALL Java_nsu_Methods_allocateStructure(JNIEnv *env, jobject obj) {
-//
-//};
-//
-//JNIEXPORT jint JNICALL Java_nsu_Methods_getStructureField(JNIEnv *env, jobject obj, jlong pointer) {
-//
-//};
-//
-//JNIEXPORT void JNICALL Java_nsu_Methods_freeMemory(JNIEnv *env, jobject obj, jlong pointer) {
-//
-//};
+typedef struct {
+    int field;
+} SomeStruct;
+
+JNIEXPORT jlong JNICALL Java_nsu_Methods_allocateStructure(JNIEnv *env, jobject obj) {
+    SomeStruct *val = (SomeStruct*) malloc(sizeof(SomeStruct));
+    val->field = 10;
+    return (jlong) val;
+};
+
+JNIEXPORT jint JNICALL Java_nsu_Methods_getStructureField(JNIEnv *env, jobject obj, jlong pointer) {
+    if ((void *)pointer == NULL) {
+        return -1;
+    }
+    SomeStruct *val = (SomeStruct*) pointer;
+    printf("Value of struct from C: %d\n", val->field);
+    return val->field;
+};
+
+JNIEXPORT void JNICALL Java_nsu_Methods_freeMemory(JNIEnv *env, jobject obj, jlong pointer) {
+    void *p = (void *)pointer;
+    if (p == NULL) {
+            return;
+        }
+    free(p);
+};
--- Task_3/src/main/java/nsu/Main.java
+++ Task_3/src/main/java/nsu/Main.java
@@ -2,39 +2,142 @@ package nsu;
 
 import com.sun.jdi.NativeMethodException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
 public class Main {
-    public static void main(String[] args) {
+    private record StreamGobbler(InputStream inputStream, Consumer<String> consumer) implements Runnable {
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    public static void execCommand(String command) {
+        long pid = ProcessHandle.current().pid();
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+            ExecutorService es = Executors.newSingleThreadExecutor();
+            es.submit(streamGobbler);
+            es.shutdown();
+            es.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void memoryEater() {
+        Methods methods = new Methods();
+        try {
+            methods.memoryEater();
+        } catch (Throwable t) {
+            // сюда не зайдёт потому что native код использует свою кучу
+            t.printStackTrace();
+        }
+    }
+
+    public static void allocateOneKB() {
         Methods methods = new Methods();
 
-//        try {
-//            methods.memoryEater();
-//        } catch (OutOfMemoryError e) {
-//            // сюда не зайдёт потому что native код использует свою кучу
-//            System.err.println("Memory limit exceeded");
+//        System.out.println("Max: " + Runtime.getRuntime().maxMemory());
+//        System.out.println("Total: " + Runtime.getRuntime().totalMemory());
+//        System.out.println("Free: " + Runtime.getRuntime().freeMemory());
+
+        long pid = ProcessHandle.current().pid();
+        execCommand("jcmd " + pid + " VM.native_memory baseline");
+        long pointer = methods.allocateOneKB();
+        System.out.println("Pointer: " + Long.toHexString(pointer));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        execCommand("jcmd " + pid + " VM.native_memory detail.diff");
+
+//        System.out.println("Max: " + Runtime.getRuntime().maxMemory());
+//        System.out.println("Total: " + Runtime.getRuntime().totalMemory());
+//        System.out.println("Free: " + Runtime.getRuntime().freeMemory());
+
+//        while (true) {
+//            try {
+//                Thread.sleep(100000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
 //        }
+    }
 
-//        System.out.println("Max: " + Runtime.getRuntime().maxMemory());
-//        System.out.println("Total: " + Runtime.getRuntime().totalMemory());
-//        System.out.println("Free: " + Runtime.getRuntime().freeMemory());
-//        long pointer = methods.allocateOneKB();
-//        System.out.println("Pointer: " + pointer);
-//        System.out.println("Max: " + Runtime.getRuntime().maxMemory());
-//        System.out.println("Total: " + Runtime.getRuntime().totalMemory());
-//        System.out.println("Free: " + Runtime.getRuntime().freeMemory());
+    public static void brokenChainOfMethods() {
+        Methods methods = new Methods();
 
+//        long pid = ProcessHandle.current().pid();
+//        execCommand("jstack " + pid + " > threadDump.txt");
         try {
             methods.brokenChainOfMethods();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
-//        SomeClass someClass = new SomeClass();
-//        methods.callObjectMethod(someClass);
+    public static void stringLength() {
+        Methods methods = new Methods();
+        String string = "Hello";
+        int length = methods.stringLength(string);
+        System.out.println("String: " + string + "\nLength: " + length);
+    }
 
-//        SomeClass obj = new SomeClass();
-//        System.out.println("Old: " + obj.field);
-//        methods.changeObjectField(obj, 4);
-//        System.out.println("New: " + obj.field);
+    public static void callObjectMethod() {
+        Methods methods = new Methods();
+        SomeClass obj = new SomeClass();
+        methods.callObjectMethod(obj);
+    }
 
+    public static void changeObjectField() {
+        Methods methods = new Methods();
+
+        SomeClass obj = new SomeClass();
+        System.out.println("Old: " + obj.field);
+        methods.changeObjectField(obj, 4);
+        System.out.println("New: " + obj.field);
+    }
+
+    public static long allocateStructure() {
+        Methods methods = new Methods();
+        long pointer = methods.allocateStructure();
+        System.out.println("Pointer: " + pointer);
+        return pointer;
+    }
+
+    public static void getStructureField(long pointer) {
+        Methods methods = new Methods();
+        int field = methods.getStructureField(pointer);
+        System.out.println("Value of struct from Java: " + field);
+    }
+
+    public static void freeMemory(long pointer) {
+        Methods methods = new Methods();
+        methods.freeMemory(pointer);
+    }
+
+    public static void main(String[] args) {
+//        memoryEater();
+//        allocateOneKB();
+//        brokenChainOfMethods();
+//        stringLength();
+//        callObjectMethod();
+//        changeObjectField();
+
+//        long pointer = allocateStructure();
+//        getStructureField(pointer);
+//        freeMemory(pointer);
     }
 }
